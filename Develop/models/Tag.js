e44131880d158874bb5df1ed371495927e2494a4
Nish Mitchell <shanishamit@gmail.com>
2021-06-01 19:16:13 -0400

    defining columns for models product and tag

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,7 +7,21 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    // * `id` * Integer. * Doesn't allow null values.* Set as primary key.* Uses auto increment.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
   },
+
+  // * `tag_name` * String.
+
+  tag_name: {
+    type: DataTypes.STRING
+  },
+
+},
   {
     sequelize,
     timestamps: false,

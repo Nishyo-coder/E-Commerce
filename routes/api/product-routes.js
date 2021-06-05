1a19b0154a35ac8a8a551f438d89316df775931a
Nish Mitchell <shanishamit@gmail.com>
2021-06-05 18:21:28 -0400

    reworking the product create

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -73,14 +73,22 @@ Product.create({
 // // update product
 // router.put('/:id', (req, res) => {
 //   // update product data
-//   Product.update(req.body, {
+//   Product.update(
+//     {
+//     product_name: req.body.product_name,
+//     price: req.body.price,
+//     stock: req.body.stock,
+//     category_id: req.body.category_id, 
+//   },
+//   {
 //     where: {
 //       id: req.params.id,
 //     },
 //   })
 //     .then((product) => {
+//       res.json(product)
 //       // find all associated tags from ProductTag
-//       return ProductTag.findAll({ where: { product_id: req.params.id } });
+//       // return ProductTag.findAll({ where: { product_id: req.params.id } });
 //     })
 //     .then((productTags) => {
 //       // get list of current tag_ids

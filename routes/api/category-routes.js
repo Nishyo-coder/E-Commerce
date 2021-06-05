024012ce623f0325837fa42f6f6121b4ad0c5b5d
Nish Mitchell <shanishamit@gmail.com>
2021-06-05 15:34:33 -0400

    insomnia request returning with no errors for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,61 +24,64 @@ router.get('/', async (req, res) => {
 });
 
 
-// router.get('/:id', (req, res) => {
-//   // find one category by its `id` value
-//   // be sure to include its associated Products
-//   Category.findByPk(req.params.id).then((CategoryData) => {
-//     res.json(CategoryData);
-//   });
-// });
+router.get('/:id', async (req, res) => {
+  try {
+    const CategoryData = await Category.findByPk(req.params.id, {
+      include: [ { model: Product } ],
+    });
+   
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // router.post('/', (req, res) => {
-//   // create a new category
-//   router.post('/', (req, res) => {
-//     Category.create(req.body)
-//       .then((newCategory) => {
-//         res.json(newCategory);
-//       })
-//       .catch((err) => {
-//         res.json(err);
-//       });
+  // create a new category
+  router.post('/', (req, res) => {
+    Category.create(req.body)
+      .then((newCategory) => {
+        res.json(newCategory);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-// router.put('/:id', (req, res) => {
-//   // update a category by its `id` value
-//     Category.update(
-//       {
-//         // All the fields you can update and the data attached to the request body.
-//         category_name: req.body.category_name,
-//         id: req.body.id,
-//       },
+  router.put('/:id', async (req, res) => {
+    //Calls the update method on the Book model
+    const updatedCategory = await Category.update(
+      {
+        // All the fields you can update and the data attached to the request body.
+        category_name: req.body.category_name,
+      },
+      {
+        // Gets a book based on the book_id given in the request parameters
+        where: {
+          category_id: req.params.category_id,
+        },
+      }
+    );
+    
+    res.json(updatedCategory);
+  });
 
-//       {
-//         // Gets the category based on the id given in the request parameters
-//         where: {
-//           id: req.params.id,
-//         },
-//       }
-//     )
-//       .then((updatedCategory) => {
-//         // Sends the updated category as a json response
-//         res.json(updatedCategory);
-//       })
-//       .catch((err) => res.json(err));
-//   });
-// });
-
-// router.delete('/:id', (req, res) => {
-//   // delete a category by its `id` value
-//     Category.destroy({
-//         where: {
-//           id: req.params.id
-//         },
-//       })
-//         .then((deletedCategory) => {
-//           res.json(deletedCategory);
-//         })
-//         .catch((err) => res.json(err));
-//     });
-// })
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+    Category.destroy({
+        where: {
+          category_id: req.params.category_id
+        },
+      })
+        .then((deletedCategory) => {
+          res.json(deletedCategory);
+        })
+        .catch((err) => res.json(err));
+    });
 
 module.exports = router;
